c983c07323a8ff7e3a0a16b7a1e1e4970a4ea1fe
Caio <caioevandro0@gmail.com>
2023-09-22 09:18:45 -0300

    db.sql adicionado e e projeto iniciado

--- api/main.go
+++ api/main.go
@@ -1,10 +1,46 @@
 package main
 
-import  (
+//"text/template"
+//"github.com/go-sql-driver/mysql"
+
+import (
+	"database/sql"
 	"fmt"
-	
+	"net/http"
 )
 
+type Names struct {
+	Id    int
+	Name  string
+	Email string
+}
+
+func dbconn(db *sql.DB) {
+	dbDriver := "mysql"
+	dbUser := ""
+	dbPass := ""
+	dbName := ""
+
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"+ @/"+dbName)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return db
+
+}
+
 func main() {
-	fmt.Println("vai ter uma api aqui, algum dia")
+	fmt.Println("Server iniciado na porta http://localhost:8000")
+
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/show", Show)
+	http.HandleFunc("/new", New)
+	http.HandleFunc("/edit", Edit)
+
+	http.HandleFunc("/insert", Insert)
+	http.HandleFunc("/update", Update)
+	http.HandleFunc("/delete", Delete)
+
+	http.ListenAndServe(":8000", nil)
 }
